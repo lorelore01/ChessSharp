6a361e9ad584edb322d35473c4caf0d055540e6a
Lorenzo <lorenzodonato75@Gmail.com>
2025-09-21 17:24:24 -0300

    chore: setting up Program.cs

---
+++ chess-sharp/Program.cs
@@ -0,0 +1,13 @@
+﻿using ChessSharp.Board;
+
+namespace ChessSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Position pos = new Position(3, 4);
+            Console.WriteLine(pos.Row);
+        }
+    }
+}
