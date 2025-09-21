8bc32e08256df891fc2c3e71a44c9a9eb5298849
Lorenzo <lorenzodonato75@gmail.com>
2025-09-21 17:35:17 -0300

    fix: corrected fatal crash

--- chess-sharp/Table/Position.cs
+++ chess-sharp/Table/Position.cs
@@ -1,21 +1,16 @@
-// O namespace reflete a estrutura: NomeDoProjeto.NomeDaPasta
 namespace ChessSharp.Board
 {
-    // A classe Position pertence a este namespace
     class Position
     {
-        // Propriedades da classe
         public int Row { get; set; }
         public int Column { get; set; }
 
-        // Construtor
         public Position(int row, int column)
         {
             Row = row;
             Column = column;
         }
 
-        // Você pode adicionar outros métodos aqui depois, como um ToString() para facilitar a impressão
         public override string ToString()
         {
             return $"{Row}, {Column}";
